d16517860e4c5bfee99185ad78b524e2ca7cb44f
JangChorong <sweatpotato13@gmail.com>
2020-12-13 13:14:01 +0900

    feat: Make to codeforces badge prototype

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -26,7 +26,7 @@ export class ApiService {
         const obj = await this.getCodeforcesInfo(handle);
         const rank = obj["result"][0]["rank"];
         const rating = obj["result"][0]["rating"];
-        return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="140" height="20" role="img"
+        return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="181" height="20" role="img"
         aria-label="codeforces">
         <title>codeforces</title>
         <linearGradient id="s" x2="0" y2="100%">
@@ -34,19 +34,23 @@ export class ApiService {
             <stop offset="1" stop-opacity=".1" />
         </linearGradient>
         <clipPath id="r">
-            <rect width="140" height="20" rx="3" fill="#fff" />
+            <rect width="181" height="20" rx="3" fill="#fff" />
         </clipPath>
         <g clip-path="url(#r)">
             <rect width="69" height="20" fill="#555" />
             <rect x="69" width="71" height="20" fill="#4c1" />
-            <rect width="140" height="20" fill="url(#s)" />
+            <rect x="140" width="41" height="20" fill="#555" />
+            <rect width="181" height="20" fill="url(#s)" />
         </g>
         <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif"
-            text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="355" y="150" fill="#010101"
-                fill-opacity=".3" transform="scale(.1)" textLength="590">codeforces</text><text x="355" y="140"
-                transform="scale(.1)" fill="#fff" textLength="590">codeforces</text><text aria-hidden="true" x="1035" y="150"
-                fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="610">${handle}</text><text x="1035"
-                y="140" transform="scale(.1)" fill="#fff" textLength="610">${handle}</text></g>
+            text-rendering="geometricPrecision" font-size="110">
+            <text aria-hidden="true" x="355" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="590">codeforces</text>
+                <text x="355" y="140" transform="scale(.1)" fill="#fff" textLength="590">codeforces</text>
+                <text aria-hidden="true" x="1035" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="610">${handle}</text>
+                <text x="1035" y="140" transform="scale(.1)" fill="#fff" textLength="610">${handle}</text>
+                <text aria-hidden="true" x="1600" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="310">${rating}</text>
+                <text x="1600" y="140" transform="scale(.1)" fill="#fff" textLength="310">${rating}</text>
+        </g>
     </svg>`;
     }
 }
